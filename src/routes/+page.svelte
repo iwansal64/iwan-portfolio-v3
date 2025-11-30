19e09821cc36f6d40d352b78c8865f2f912bf2dc
Iwansal64 <iwansal64@gmail.com>
2025-11-30 14:48:47 +0700

    [new] First Initialization

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+      <title>Iwan's Portfolio</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+</script>
+
