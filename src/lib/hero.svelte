b4303cb95ad0a84e0607ef842e280c9bfb22e26f
Iwansal64 <iwansal64@gmail.com>
2025-12-01 09:12:26 +0700

    [new] Add Hero Section

---
+++ src/lib/hero.svelte
@@ -0,0 +1,26 @@
+<div id="hero" class="w-screen min-h-screen h-full">
+      <div class="flex flex-row gap-4 mt-48 w-[80%] mx-auto *:w-full">
+            <div class="flex flex-col py-24">
+                  <h1 class="text-6xl font-bold uppercase">Ridwan</h1>
+                  <div class="flex flex-row gap-2 items-center">
+                        <div class="w-8 h-[2px] bg-white"></div>
+                        <p class="text-2xl italic">Passionate Fullstack Developer</p>
+                  </div>
+                  <div class="flex mt-4 flex-row justify-between gap-2">
+                        <img src="/src/lib/assets/svelte-icon.png" alt="">
+                        <img src="/src/lib/assets/nodejs-icon.png" alt="">
+                        <img src="/src/lib/assets/react-icon.png" alt="">
+                        <img src="/src/lib/assets/rust-icon.png" alt="">
+                        <img src="/src/lib/assets/next-icon.png" alt="">
+                        <img src="/src/lib/assets/mysql-icon.png" alt="">
+                        <img src="/src/lib/assets/mongodb-icon.png" alt="">
+                        <img src="/src/lib/assets/postgres-icon.png" alt="">
+                  </div>
+            </div>
+            <div class="flex justify-center items-center">
+                  <div class="w-4/5 aspect-square overflow-hidden rounded-4xl">
+                        <img src="/src/lib/assets/hero.jpg" class="w-full h-full object-cover" alt="">
+                  </div>
+            </div>
+      </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,8 @@
 
 <script>
       import Navbar from "$lib/navbar.svelte";
+      import Hero from "$lib/hero.svelte";
 </script>
 
 <Navbar />
+<Hero />
